30612adbb2ea3c152809b3563c7ea6fa431a4107
mahib-bit <sadmanmahib@gmail.com>
2026-02-28 05:17:33 +0600

    restaring

---
+++ conditionals.js
@@ -0,0 +1,4 @@
+// console.log(5 > 3); // true
+// console.log(5 < 3); // false
+// console.log(5 == 15); // false
+console.log(5 != 15); // true
---
+++ if.js
@@ -0,0 +1,7 @@
+if (5 > 3) {
+    console.log("5 is greater than 3");
+}
+age = 20;
+if (age >= 18) {
+    console.log("You are an adult.");
+}
