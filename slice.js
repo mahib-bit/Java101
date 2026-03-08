f0c5cbc6c98c78f3d096f67c48d740be9650bf9d
mahib-bit <sadmanmahib@gmail.com>
2026-03-08 20:00:42 +0600

    add

---
+++ slice.js
@@ -0,0 +1,19 @@
+const address =  'Attolika';
+const part = address.slice(2, 6);
+console.log(part);
+
+const sentence = 'You are a good person';
+console.log(sentence.split('a'));
+
+const name = ['Rahim', 'Karim', 'Rahman', 'Karim', 'Rahim', 'Karim'];
+console.log(name.join('|'));
+
+const hollow = 'Red';
+const purple = 'yellow';
+
+const newColor = hollow + purple;
+console.log(newColor);
+
+// includes
+
+console.log(hollow.includes('e'));
