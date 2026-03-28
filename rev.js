b1c5d1b4a95995dae22c78beb17e13c70ed8e470
mahib-bit <sadmanmahib@gmail.com>
2026-03-29 00:41:32 +0600

    new

---
+++ rev.js
@@ -0,0 +1,19 @@
+/* JavaScript Variables
+1. const --> values wont change
+2. let --> values can change
+3. var --> should not use (unless exceptional cases)
+*/
+
+const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]); 
+}
+
+const prices = [10, 20, 30, 40, 50];
+
+let k=0;
+while(k <prices.length) {
+    console.log(prices[k]);
+    k++;
+}
