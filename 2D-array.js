5c1a50a34003435b0181b290d233e642ba681d91
mahib-bit <sadmanmahib@gmail.com>
2026-03-29 23:56:34 +0600

    pps

---
+++ 2D-array.js
@@ -0,0 +1,9 @@
+const exam_marks = [
+    [78, 85, 90],
+    [92, 88, 95],
+    [80, 82, 87]
+];
+
+console.log(exam_marks);
+console.log(exam_marks[0]);
+console.log(exam_marks[1][2]);
---
+++ function.js
@@ -0,0 +1,7 @@
+
+function square(number){
+    console.log(`Value of parameter:`,number);
+    const borgo = number * number;
+    console.log(`The square of the number is:`,borgo);
+}
+square(4);
